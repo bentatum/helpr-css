8209722781c8b8d8b73aed00bbc60ba3fc81f67f
Benjamin Tatum <mail@benjtatum.com>
2020-03-10 14:38:52 -0400

    v1.1.0 - position element, starting with .sticky and .sticky--{top|right|bottom|left} - improved container class variable declaration pattern

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
-for element in "borders" "colors" "flexbox" "grid" "overflow" "reset" "screen" "shadows" "shape" "sizes" "spacing" "text"; do
+for element in "borders" "colors" "flexbox" "grid" "overflow" "position" "reset" "screen" "shadows" "shape" "sizes" "spacing" "text"; do
     sass src/elements/$element.sass elements/$element.min.css -s compressed
     sass src/elements/$element.sass elements/$element.css --no-source-map
 done
@@ -8,7 +8,7 @@ for varset in "borders" "colors" "screen" "shadows" "spacing" "text"; do
     sass src/variables/$varset.sass variables/$varset.css --no-source-map
 done
 
-for export in "borders" "color" "flexbox" "grid" "default" "overflow" "screen" "shadows" "shape" "sizes" "spacing" "text"; do
+for export in "borders" "color" "flexbox" "grid" "default" "overflow" "position" "screen" "shadows" "shape" "sizes" "spacing" "text"; do
     sass src/exports/$export.sass $export.min.css -s compressed
     sass src/exports/$export.sass $export.css --no-source-map
 done
